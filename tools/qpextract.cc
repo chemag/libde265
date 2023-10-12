96d97cae393406b08628e629f24b1502cebdf35e
Chema Gonzalez <chemag@meta.com>
2023-10-12 12:13:59 -0700

    tools: qpextract: lint code
    Simple clang-format:
    ```
    $ clang-format -i -style=google tools/qpextract.cc
    ```

--- tools/qpextract.cc
+++ tools/qpextract.cc
@@ -30,10 +30,11 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <limits>
-#include <getopt.h>
 #ifdef HAVE_MALLOC_H
 #include <malloc.h>
 #endif
@@ -46,65 +47,58 @@
 
 #include "libde265/quality.h"
 
-
 #define BUFFER_SIZE 40960
 
-bool nal_input=false;
-bool check_hash=false;
-bool show_help=false;
-bool logging=true;
-bool no_acceleration=false;
-const char *bytestream_filename;
+bool nal_input = false;
+bool check_hash = false;
+bool show_help = false;
+bool logging = true;
+bool no_acceleration = false;
+const char* bytestream_filename;
 const char* reference_filename;
 int highestTID = 100;
 int maxQP = 52;
 int minQP = 0;
-int verbosity=0;
-int disable_deblocking=0;
-int disable_sao=0;
+int verbosity = 0;
+int disable_deblocking = 0;
+int disable_sao = 0;
 char* infile = NULL;
 char* outfile = NULL;
 FILE* fin = NULL;
 FILE* fout = NULL;
 
 static struct option long_options[] = {
-  {"check-hash", no_argument,       0, 'c' },
-  {"profile",    no_argument,       0, 'p' },
-  {"frames",     required_argument, 0, 'f' },
-  {"infile",     required_argument, 0, 'i' },
-  {"outfile",     required_argument, 0, 'o' },
-  {"dump",       no_argument,       0, 'd' },
-  {"dump-image-data", no_argument,  0, 'I' },
-  {"nal",        no_argument,       0, 'n' },
-  {"no-logging", no_argument,       0, 'L' },
-  {"help",       no_argument,       0, 'h' },
-  {"noaccel",    no_argument,       0, '0' },
-  {"highest-TID", required_argument, 0, 'T' },
-  {"verbose",    no_argument,       0, 'v' },
-  {"disable-deblocking", no_argument, &disable_deblocking, 1 },
-  {"disable-sao",        no_argument, &disable_sao, 1 },
-  {"max-qp",        required_argument, 0, 'Q' },
-  {"min-qp",        required_argument, 0, 'q' },
-  {0,         0,                 0,  0 }
-};
+    {"check-hash", no_argument, 0, 'c'},
+    {"profile", no_argument, 0, 'p'},
+    {"frames", required_argument, 0, 'f'},
+    {"infile", required_argument, 0, 'i'},
+    {"outfile", required_argument, 0, 'o'},
+    {"dump", no_argument, 0, 'd'},
+    {"dump-image-data", no_argument, 0, 'I'},
+    {"nal", no_argument, 0, 'n'},
+    {"no-logging", no_argument, 0, 'L'},
+    {"help", no_argument, 0, 'h'},
+    {"noaccel", no_argument, 0, '0'},
+    {"highest-TID", required_argument, 0, 'T'},
+    {"verbose", no_argument, 0, 'v'},
+    {"disable-deblocking", no_argument, &disable_deblocking, 1},
+    {"disable-sao", no_argument, &disable_sao, 1},
+    {"max-qp", required_argument, 0, 'Q'},
+    {"min-qp", required_argument, 0, 'q'},
+    {0, 0, 0, 0}};
 
-
-
-static int width,height;
-static uint32_t framecnt=0;
-
-
-
+static int width, height;
+static uint32_t framecnt = 0;
 
 #ifdef HAVE___MALLOC_HOOK
 #ifdef __GNUC__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #endif
-static void *(*old_malloc_hook)(size_t, const void *);
+static void* (*old_malloc_hook)(size_t, const void*);
 
-static void *new_malloc_hook(size_t size, const void *caller) {
-  void *mem;
+static void* new_malloc_hook(size_t size, const void* caller) {
+  void* mem;
 
   /*
   if (size>1000000) {
@@ -133,61 +127,46 @@ void (*volatile __malloc_initialize_hook)(void) = init_my_hooks;
 #endif
 #endif
 
+void dump_vps(video_parameter_set* vps) { vps->dump(STDOUT_FILENO); }
 
-void dump_vps(video_parameter_set* vps)
-{
-  vps->dump(STDOUT_FILENO);
-}
+void dump_sps(seq_parameter_set* sps) { sps->dump(STDOUT_FILENO); }
 
-void dump_sps(seq_parameter_set* sps)
-{
-  sps->dump(STDOUT_FILENO);
-}
+void dump_pps(pic_parameter_set* pps) { pps->dump(STDOUT_FILENO); }
 
-void dump_pps(pic_parameter_set* pps)
-{
-  pps->dump(STDOUT_FILENO);
-}
-
-void get_qp_distro(const de265_image* img, int* qp_distro)
-{
+void get_qp_distro(const de265_image* img, int* qp_distro) {
   const seq_parameter_set& sps = img->get_sps();
   int minCbSize = sps.MinCbSizeY;
 
   // init QP distro
-  for (int q=0;q<100;q++)
-    qp_distro[q] = 0;
+  for (int q = 0; q < 100; q++) qp_distro[q] = 0;
 
   // update QP distro
-  for (int y0=0;y0<sps.PicHeightInMinCbsY;y0++)
-    {
-    for (int x0=0;x0<sps.PicWidthInMinCbsY;x0++)
-      {
-        int log2CbSize = img->get_log2CbSize_cbUnits(x0,y0);
-        if (log2CbSize==0) {
-          continue;
-        }
-
-        int xb = x0*minCbSize;
-        int yb = y0*minCbSize;
-
-        int CbSize = 1<<log2CbSize;
-        int qp = img->get_QPY(xb,yb);
-        if (qp < 0 || qp >= 100) {
-          fprintf(stderr, "error: qp: %d\n",qp);
-          continue;
-        }
-        // TODO(chema): normalize the QP distro by CB size
-        //qp_distro[qp] += (CbSize*CbSize);
-        // provide per-block QP output
-        qp_distro[qp] += 1;
+  for (int y0 = 0; y0 < sps.PicHeightInMinCbsY; y0++) {
+    for (int x0 = 0; x0 < sps.PicWidthInMinCbsY; x0++) {
+      int log2CbSize = img->get_log2CbSize_cbUnits(x0, y0);
+      if (log2CbSize == 0) {
+        continue;
       }
+
+      int xb = x0 * minCbSize;
+      int yb = y0 * minCbSize;
+
+      int CbSize = 1 << log2CbSize;
+      int qp = img->get_QPY(xb, yb);
+      if (qp < 0 || qp >= 100) {
+        fprintf(stderr, "error: qp: %d\n", qp);
+        continue;
+      }
+      // TODO(chema): normalize the QP distro by CB size
+      // qp_distro[qp] += (CbSize*CbSize);
+      // provide per-block QP output
+      qp_distro[qp] += 1;
     }
+  }
   return;
 }
 
-void dump_image(de265_image* img)
-{
+void dump_image(de265_image* img) {
 #define BUFSIZE 1024
   char buffer[BUFSIZE] = {};
   int bi = 0;
@@ -197,11 +176,11 @@ void dump_image(de265_image* img)
   get_qp_distro(img, qp_distro);
 
   // dump QP distro
-  bi += snprintf(buffer+bi, BUFSIZE-bi, "%i,", img->get_ID());
+  bi += snprintf(buffer + bi, BUFSIZE - bi, "%i,", img->get_ID());
   for (int qp = minQP; qp <= maxQP; qp++) {
-    bi += snprintf(buffer+bi, BUFSIZE-bi, "%i,", qp_distro[qp]);
+    bi += snprintf(buffer + bi, BUFSIZE - bi, "%i,", qp_distro[qp]);
   }
-  buffer[bi-1] = '\n';
+  buffer[bi - 1] = '\n';
   fprintf(fout, buffer);
 
   if (!verbosity) {
@@ -212,91 +191,121 @@ void dump_image(de265_image* img)
   const seq_parameter_set& sps = img->get_sps();
   int minCbSize = sps.MinCbSizeY;
 
-  for (int y0=0;y0<sps.PicHeightInMinCbsY;y0++)
-    {
-    for (int x0=0;x0<sps.PicWidthInMinCbsY;x0++)
-      {
-      int log2CbSize = img->get_log2CbSize_cbUnits(x0,y0);
-      if (log2CbSize==0) {
+  for (int y0 = 0; y0 < sps.PicHeightInMinCbsY; y0++) {
+    for (int x0 = 0; x0 < sps.PicWidthInMinCbsY; x0++) {
+      int log2CbSize = img->get_log2CbSize_cbUnits(x0, y0);
+      if (log2CbSize == 0) {
         continue;
       }
 
-      int xb = x0*minCbSize;
-      int yb = y0*minCbSize;
+      int xb = x0 * minCbSize;
+      int yb = y0 * minCbSize;
 
-      int CbSize = 1<<log2CbSize;
+      int CbSize = 1 << log2CbSize;
 
-      int q = img->get_QPY(xb,yb);
+      int q = img->get_QPY(xb, yb);
       if (q < 0 || q >= 100) {
-        fprintf(stderr, "error: q: %d\n",q);
+        fprintf(stderr, "error: q: %d\n", q);
         continue;
       }
       // provide per-block QP output
-      fprintf(stderr, "id: %i qp_coord[%i,%i]: %i CbSize: %i\n", img->get_ID(), xb, yb, q, CbSize);
-      }
+      fprintf(stderr, "id: %i qp_coord[%i,%i]: %i CbSize: %i\n", img->get_ID(),
+              xb, yb, q, CbSize);
     }
+  }
 
   fprintf(fout, buffer);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   while (1) {
     int option_index = 0;
 
-    int c = getopt_long(argc, argv, "qt:chf:i:o:dILB:n0vT:m:se"
-                        , long_options, &option_index);
-    if (c == -1)
-      break;
+    int c = getopt_long(argc, argv, "qt:chf:i:o:dILB:n0vT:m:se", long_options,
+                        &option_index);
+    if (c == -1) break;
 
     switch (c) {
-    case 'c': check_hash=true; break;
-    case 'h': show_help=true; break;
-    case 'n': nal_input=true; break;
-    case 'L': logging=false; break;
-    case '0': no_acceleration=true; break;
-    case 'T': highestTID=atoi(optarg); break;
-    case 'Q': maxQP=atoi(optarg); break;
-    case 'q': minQP=atoi(optarg); break;
-    case 'v': verbosity++; break;
-    case 'i': infile=optarg; break;
-    case 'o': outfile=optarg; break;
+      case 'c':
+        check_hash = true;
+        break;
+      case 'h':
+        show_help = true;
+        break;
+      case 'n':
+        nal_input = true;
+        break;
+      case 'L':
+        logging = false;
+        break;
+      case '0':
+        no_acceleration = true;
+        break;
+      case 'T':
+        highestTID = atoi(optarg);
+        break;
+      case 'Q':
+        maxQP = atoi(optarg);
+        break;
+      case 'q':
+        minQP = atoi(optarg);
+        break;
+      case 'v':
+        verbosity++;
+        break;
+      case 'i':
+        infile = optarg;
+        break;
+      case 'o':
+        outfile = optarg;
+        break;
     }
   }
 
   if (show_help) {
-    fprintf(stderr,"# qpextract  v%s\n", de265_get_version());
-    fprintf(stderr,"usage: qpextract [options] -i videofile.bin [-o output.csv]\n");
-    fprintf(stderr,"The video file must be a raw bitstream, or a stream with NAL units (option -n).\n");
-    fprintf(stderr,"\n");
-    fprintf(stderr,"options:\n");
-    fprintf(stderr,"  -c, --check-hash  perform hash check\n");
-    fprintf(stderr,"  -n, --nal         input is a stream with 4-byte length prefixed NAL units\n");
-    fprintf(stderr,"  -d, --dump        dump headers\n");
-    fprintf(stderr,"  -T, --highest-TID select highest temporal sublayer to decode\n");
-    fprintf(stderr,"      --disable-deblocking   disable deblocking filter\n");
-    fprintf(stderr,"      --disable-sao          disable sample-adaptive offset filter\n");
-    fprintf(stderr,"  -h, --help        show help\n");
+    fprintf(stderr, "# qpextract  v%s\n", de265_get_version());
+    fprintf(stderr,
+            "usage: qpextract [options] -i videofile.bin [-o output.csv]\n");
+    fprintf(stderr,
+            "The video file must be a raw bitstream, or a stream with NAL "
+            "units (option -n).\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "options:\n");
+    fprintf(stderr, "  -c, --check-hash  perform hash check\n");
+    fprintf(stderr,
+            "  -n, --nal         input is a stream with 4-byte length prefixed "
+            "NAL units\n");
+    fprintf(stderr, "  -d, --dump        dump headers\n");
+    fprintf(stderr,
+            "  -T, --highest-TID select highest temporal sublayer to decode\n");
+    fprintf(stderr, "      --disable-deblocking   disable deblocking filter\n");
+    fprintf(
+        stderr,
+        "      --disable-sao          disable sample-adaptive offset filter\n");
+    fprintf(stderr, "  -h, --help        show help\n");
 
     exit(show_help ? 0 : 5);
   }
 
-
   // create and configure decoder
   de265_error err = DE265_OK;
   de265_decoder_context* ctx = de265_new_decoder();
-  de265_set_parameter_bool(ctx, DE265_DECODER_PARAM_BOOL_SEI_CHECK_HASH, check_hash);
-  de265_set_parameter_bool(ctx, DE265_DECODER_PARAM_SUPPRESS_FAULTY_PICTURES, false);
-  de265_set_parameter_bool(ctx, DE265_DECODER_PARAM_DISABLE_DEBLOCKING, disable_deblocking);
+  de265_set_parameter_bool(ctx, DE265_DECODER_PARAM_BOOL_SEI_CHECK_HASH,
+                           check_hash);
+  de265_set_parameter_bool(ctx, DE265_DECODER_PARAM_SUPPRESS_FAULTY_PICTURES,
+                           false);
+  de265_set_parameter_bool(ctx, DE265_DECODER_PARAM_DISABLE_DEBLOCKING,
+                           disable_deblocking);
   de265_set_parameter_bool(ctx, DE265_DECODER_PARAM_DISABLE_SAO, disable_sao);
 
-  //if (dump_headers) {
-  //  de265_set_parameter_int(ctx, DE265_DECODER_PARAM_DUMP_SEI, 1);
-  //  de265_set_parameter_int(ctx, DE265_DECODER_PARAM_DUMP_SLICE_HEADERS, 1);
-  //}
+  // if (dump_headers) {
+  //   de265_set_parameter_int(ctx, DE265_DECODER_PARAM_DUMP_SEI, 1);
+  //   de265_set_parameter_int(ctx, DE265_DECODER_PARAM_DUMP_SLICE_HEADERS, 1);
+  // }
 
   if (no_acceleration) {
-    de265_set_parameter_int(ctx, DE265_DECODER_PARAM_ACCELERATION_CODE, de265_acceleration_SCALAR);
+    de265_set_parameter_int(ctx, DE265_DECODER_PARAM_ACCELERATION_CODE,
+                            de265_acceleration_SCALAR);
   }
 
   if (!logging) {
@@ -304,7 +313,6 @@ int main(int argc, char** argv)
   }
 
   de265_set_verbosity(verbosity);
-
 
   de265_set_limit_TID(ctx, highestTID);
 
@@ -328,8 +336,8 @@ int main(int argc, char** argv)
   } else {
     // open the input file
     fin = fopen(infile, "rb");
-    if (fin==NULL) {
-      fprintf(stderr,"cannot open file %s!\n", infile);
+    if (fin == NULL) {
+      fprintf(stderr, "cannot open file %s!\n", infile);
       exit(10);
     }
   }
@@ -340,8 +348,8 @@ int main(int argc, char** argv)
   } else {
     // open the output file
     fout = fopen(outfile, "wb");
-    if (fout==NULL) {
-      fprintf(stderr,"cannot open file %s!\n", outfile);
+    if (fout == NULL) {
+      fprintf(stderr, "cannot open file %s!\n", outfile);
       exit(10);
     }
   }
@@ -350,78 +358,74 @@ int main(int argc, char** argv)
 #define BUFSIZE 1024
   char buffer[BUFSIZE] = {};
   int bi = 0;
-  bi += snprintf(buffer+bi, BUFSIZE-bi, "frame,");
+  bi += snprintf(buffer + bi, BUFSIZE - bi, "frame,");
   for (int qp = minQP; qp <= maxQP; qp++) {
-    bi += snprintf(buffer+bi, BUFSIZE-bi, "%i,", qp);
+    bi += snprintf(buffer + bi, BUFSIZE - bi, "%i,", qp);
   }
-  buffer[bi-1] = '\n';
+  buffer[bi - 1] = '\n';
   fprintf(fout, buffer);
 
-  bool stop=false;
+  bool stop = false;
 
-  int pos=0;
+  int pos = 0;
 
-  while (!stop)
-    {
-      if (nal_input) {
-        uint8_t len[4];
-        int n = fread(len,1,4,fin);
-        int length = (len[0]<<24) + (len[1]<<16) + (len[2]<<8) + len[3];
+  while (!stop) {
+    if (nal_input) {
+      uint8_t len[4];
+      int n = fread(len, 1, 4, fin);
+      int length = (len[0] << 24) + (len[1] << 16) + (len[2] << 8) + len[3];
 
-        uint8_t* buf = (uint8_t*)malloc(length);
-        n = fread(buf,1,length,fin);
-        err = de265_push_NAL(ctx, buf,n,  pos, (void*)1);
-        free(buf);
-        pos+=n;
-      }
-      else {
-        // read a chunk of input data
-        uint8_t buf[BUFFER_SIZE];
-        int n = fread(buf,1,BUFFER_SIZE,fin);
+      uint8_t* buf = (uint8_t*)malloc(length);
+      n = fread(buf, 1, length, fin);
+      err = de265_push_NAL(ctx, buf, n, pos, (void*)1);
+      free(buf);
+      pos += n;
+    } else {
+      // read a chunk of input data
+      uint8_t buf[BUFFER_SIZE];
+      int n = fread(buf, 1, BUFFER_SIZE, fin);
 
-        // decode input data
-        if (n) {
-          err = de265_push_data(ctx, buf, n, pos, (void*)2);
-          if (err != DE265_OK) {
-            break;
-          }
+      // decode input data
+      if (n) {
+        err = de265_push_data(ctx, buf, n, pos, (void*)2);
+        if (err != DE265_OK) {
+          break;
         }
-
-        pos+=n;
       }
 
-      if (feof(fin)) {
-        err = de265_flush_data(ctx); // indicate end of stream
-        stop = true;
-      }
-
-      // decoding loop
-      int more=1;
-      while (more)
-        {
-          more = 0;
-
-          // decode some more
-          err = de265_decode(ctx, &more);
-          if (err != DE265_OK) {
-            if (check_hash && err == DE265_ERROR_CHECKSUM_MISMATCH)
-              stop = 1;
-            more = 0;
-            break;
-          }
-
-          // show available images
-          const de265_image* img = de265_get_next_picture(ctx);
-
-          // show warnings
-          for (;;) {
-            de265_error warning = de265_get_warning(ctx);
-            if (warning==DE265_OK) {
-              break;
-            }
-          }
-        }
+      pos += n;
     }
+
+    if (feof(fin)) {
+      err = de265_flush_data(ctx);  // indicate end of stream
+      stop = true;
+    }
+
+    // decoding loop
+    int more = 1;
+    while (more) {
+      more = 0;
+
+      // decode some more
+      err = de265_decode(ctx, &more);
+      if (err != DE265_OK) {
+        if (check_hash && err == DE265_ERROR_CHECKSUM_MISMATCH) stop = 1;
+        more = 0;
+        break;
+      }
+
+      // show available images
+      const de265_image* img = de265_get_next_picture(ctx);
+
+      // show warnings
+      for (;;) {
+        de265_error warning = de265_get_warning(ctx);
+        if (warning == DE265_OK) {
+          break;
+        }
+      }
+    }
+  }
 
   // clean up file pointers
   fclose(fin);
@@ -429,5 +433,5 @@ int main(int argc, char** argv)
 
   de265_free_decoder(ctx);
 
-  return err==DE265_OK ? 0 : 10;
+  return err == DE265_OK ? 0 : 10;
 }
